d5a46b9d08a1107da685c07ff257c611768ae2b7
kiraGit67 <kirsten@sais.de>
2023-02-02 10:01:42 +0100

    adds single line and multi line comments to script.js

--- 01-first-hello-world-script/script.js
+++ 01-first-hello-world-script/script.js
@@ -1,2 +1,8 @@
+// Single Line Comment: My first Hello World Script
 console.log("Hello World!");
+
+/*
+    Multi Line Comment
+    My First Hello World Script
+*/
 console.log("My First 'Hello World'-Script");
