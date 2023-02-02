138db3724727ecee8eb38d0ed824ecd1cf538589
kiraGit67 <kirsten@sais.de>
2023-02-02 19:44:24 +0100

    adds script to script.js

---
+++ 02-operators-and-data-types/script.js
@@ -0,0 +1,31 @@
+console.log("K" + "i" + "r" + "s" + "t" + "e" + "n");
+
+// Task 02 Make it work!
+
+// Error! Make it work
+console.log("Hallo");
+
+// Error! Make it work
+console.log("That doesn't work");
+
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 > 52);
+console.log(50 + 2 >= 52);
+console.log(50 + 2 === 52);
+
+// should be false. Change only the operator to fix it.
+console.log("333" == 333);
+console.log("333" === 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log(600 + 510 / 2);
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22
+console.log("2" + 2);
+console.log(2 + 2);
+console.log(+"2" + 2);
+
+// Task 03 Compare 2 conditions
+console.log(25 / 5 === 5 && 50 / 10 === 5);
+console.log(25 / 5 === 50 / 10);
