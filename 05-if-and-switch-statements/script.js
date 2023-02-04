c10cfc41d8ed0d2db528ece1d69445f1788d460d
kiraGit67 <kirsten@sais.de>
2023-02-04 15:24:58 +0100

    adds html-skeleton and script-tag to index.html

---
+++ 05-if-and-switch-statements/script.js
@@ -0,0 +1,78 @@
+// Task 01) Spot the errors and fix them
+console.log("01) Spot the errors and fix them");
+
+const size = 25;
+let result;
+
+if (size > 10 && size < 20) {
+  result = "Greater than 10";
+} else if (size > 20) {
+  result = "Greater than 20";
+} else {
+  result = "Lower than 10";
+}
+
+console.log(result);
+// Returns Greater than 10 but should return Greater than 20
+
+// Task 02) oddEven function
+console.log("02) oddEven function");
+
+function oddEven(num) {
+  if (num % 2 === 0) {
+    return "Even";
+  } else if (num % 2 !== 0) {
+    return "Odd";
+  }
+}
+
+console.log(oddEven(4));
+// result should be even
+
+console.log(oddEven(3));
+// result should be odd
+
+console.log(oddEven(-1));
+// result should be odd
+
+console.log(oddEven(10));
+// result should be even
+
+// Task 02) oddEven function
+console.log("03) oldYoung function");
+
+function oldYoung(age) {
+  if (age < 16 && age >= 0) {
+    return "children";
+  } else if (age < 50 && age >= 0 && age >= 16) {
+    return "young person";
+  } else if (age < 0 || typeof age === "string") {
+    return "invalid parameter";
+  } else {
+    return "elder person";
+  }
+}
+
+console.log(oldYoung(27));
+// result should be young person
+
+console.log(oldYoung(6));
+// result should be children
+
+console.log(oldYoung(-1));
+// result should be invalid parameter
+
+console.log(oldYoung(86));
+// result should be elder person
+
+console.log(oldYoung("72"));
+// result should be invalid parameter
+
+console.log(oldYoung(16));
+// result should be young person
+
+console.log(oldYoung(0));
+// result should be children
+
+console.log(oldYoung(50));
+// result should be elder person
